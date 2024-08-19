b3c8dfd21060680ec3261280ae862e985976a972
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-08-19 19:26:44 -0300

    sequelize configurado

--- 1-Fundamentos Node/4. Trabalhando com Dados/crud-node/src/model/db.ts
+++ 1-Fundamentos Node/4. Trabalhando com Dados/crud-node/src/model/db.ts
@@ -4,10 +4,10 @@ const sequelize = new Sequelize({
     username: 'root',
     password: 'root',
     database: 'database-crud-node',
-
+    
     dialect: 'postgres',
     host: 'localhost',
     port: 5432
-})
+});
 
 export default sequelize;
