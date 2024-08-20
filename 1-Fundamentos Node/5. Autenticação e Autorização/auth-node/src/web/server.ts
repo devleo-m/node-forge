3fef40642d12d256d336a39f50b097755be1fcb7
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-08-20 02:45:13 -0300

    Update server.ts

--- 1-Fundamentos Node/5. Autenticação e Autorização/auth-node/src/web/server.ts
+++ 1-Fundamentos Node/5. Autenticação e Autorização/auth-node/src/web/server.ts
@@ -1,6 +1,8 @@
-import bodyParser from 'body-parser';
 import express from 'express';
+import bodyParser from 'body-parser';
 import sequelize from '../config/database';
+import authRoutes from '../routes/authRoutes';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const app = express();
 const port = process.env.PORT;
@@ -10,6 +12,13 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.send("Hello World!!!");
 })
+
+app.use('/auth', authRoutes);
+
+// Exemplo de rota protegida
+app.get('/protected', authMiddleware, (req, res) => {
+    res.json({ message: 'Rota protegida' });
+});
 
 sequelize.sync().then(() => {
     console.log('Banco de dados conectado com sucesso!');
