c4de48e1f5bc21f03b0cdd227f723accbb3de0f6
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-08-19 22:43:52 -0300

    Update comandos.sh

--- 1-Fundamentos Node/4. Trabalhando com Dados/crud-node/comandos.sh
+++ 1-Fundamentos Node/4. Trabalhando com Dados/crud-node/comandos.sh
@@ -2,3 +2,5 @@ npm init -y
 npm install express body-parser express-validator multer pg sequelize typescript @types/node @types/express ts-node
 npm install typescript --save-dev
 npx tsc --init
+
+npx ts-node src/scripts/init-db.ts // inicializa o banco de dados
