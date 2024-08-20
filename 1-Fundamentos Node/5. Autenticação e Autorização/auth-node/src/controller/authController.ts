33d0801950c903b2d618a41528a3093152007514
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-08-20 02:36:20 -0300

    Update - auth/controller and routes

--- 1-Fundamentos Node/5. Autenticação e Autorização/auth-node/src/controller/authController.ts
+++ 1-Fundamentos Node/5. Autenticação e Autorização/auth-node/src/controller/authController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import { AuthService } from '../services/authService';
 
-export class AuthController {
-    static async register(req: Request, res: Response) {
+export namespace AuthController {
+    export const register = async (req: Request, res: Response) => {
         try {
             const { username, email, password } = req.body;
             const user = await AuthService.register(username, email, password);
@@ -12,7 +12,7 @@ export class AuthController {
         }
     }
 
-    static async login(req: Request, res: Response) {
+    export const login = async(req: Request, res: Response) => {
         try {
             const { email, password } = req.body;
             const { user, token } = await AuthService.login(email, password);
