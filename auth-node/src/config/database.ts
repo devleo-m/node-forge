fa6e095704eae702bdbac108dab96040370c05b6
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-17 01:46:00 -0300

    Create database.ts

---
+++ auth-node/src/config/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: Number(process.env.POSTGRES_PORT),
+});
+
+export default sequelize;
